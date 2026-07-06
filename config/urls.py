ff21b459de8db17bb1bb0512dad38e2e6cb193ca
Jackson <pope.jackso09@gmail.com>
2026-07-05 20:23:04 -0700

    Added home page and general set up files.

---
+++ config/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('', include('apps.core.urls')),
+    path('resources/', include('apps.resources.urls')),
+    path('opportunities/', include('apps.opportunities.urls')),
+    path('users/', include('apps.users.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './templates/**/*.html',
+    './apps/**/*.html',
+    './static/**/*.js',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'nav-bg': '#1F452D',
+        'button-bg': '#ADD6C5',
+      },
+      fontFamily: {
+        'heading': ['Inter', '-apple-system', 'Roboto', 'Helvetica', 'sans-serif'],
+        'subtitle': ['Oswald', '-apple-system', 'Roboto', 'Helvetica', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: ['light'],
+  },
+}
